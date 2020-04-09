47a67d443bc3b5d7afea1ba2eb1b58ecdb50ebe5
lipowskm <m.lipowski95@gmail.com>
2020-04-09 18:26:38 +0200

    add validators to fields in Login and Register form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,6 +3,7 @@
     <v-container fill-height>
       <v-layout row wrap justify-center>
         <v-flex xs12 sm8 md4 align-self-center>
+          <ValidationObserver ref="observer" @submit.prevent="submit()" v-slot="{ invalid }">
           <v-card class="elevation-12">
             <v-tabs centered>
               <v-tab
@@ -25,12 +26,27 @@
               <div v-if="loginButtonClicked">
                 <v-card-text>
                   <v-form @keyup.enter="submit">
-                    <v-text-field @keyup.enter="submit" v-model="username" prepend-icon="mdi-account-circle"
-                                  name="login"
+                    <ValidationProvider name="Username" rules="required" mode="aggressive">
+                    <v-text-field slot-scope="{ errors }"
+                                  :error-messages="errors"
+                                  @keyup.enter="submit"
+                                  v-model="username"
+                                  prepend-icon="mdi-account-circle"
+                                  name="username"
                                   label="Username/email"
                                   type="text"></v-text-field>
-                    <v-text-field @keyup.enter="submit" v-model="password" prepend-icon="mdi-lock" name="password"
-                                  label="Password" id="password" type="password"></v-text-field>
+                    </ValidationProvider>
+                    <ValidationProvider name="Password" rules="required" mode="aggressive">
+                    <v-text-field slot-scope="{ errors }"
+                                  :error-messages="errors"
+                                  @keyup.enter="submit"
+                                  v-model="password"
+                                  prepend-icon="mdi-lock"
+                                  name="password"
+                                  label="Password"
+                                  id="password"
+                                  type="password"></v-text-field>
+                    </ValidationProvider>
                   </v-form>
                   <div v-if="loginError">
                     <v-alert :value="loginError" transition="fade-transition" type="error">
@@ -38,7 +54,7 @@
                     </v-alert>
                   </div>
                   <v-card-actions>
-                    <v-btn @click.prevent="submit" large color="primary">Login</v-btn>
+                    <v-btn @click="submit" :disabled="invalid" color="primary">Login</v-btn>
                     <v-flex class="caption text-sm-right">
                       <router-link to="/recover-password">Forgot your password?</router-link>
                     </v-flex>
@@ -50,30 +66,55 @@
               <div v-if="registerButtonClicked">
                 <v-card-text>
                     <v-form>
-                      <ValidationProvider name="First name">
-                      <v-text-field v-model="firstName"
+                      <ValidationProvider name="First name" rules="required|min:3" mode="passive">
+                      <v-text-field slot-scope="{ errors }"
+                                    :error-messages="errors"
+                                    v-model="firstName"
                                     name="firstName"
                                     label="First name"
                                     type="text"></v-text-field>
                       </ValidationProvider>
-                      <v-text-field v-model="lastName" name="lastName"
+                      <v-text-field v-model="lastName"
+                                    name="lastName"
                                     label="Last name"
                                     type="text"></v-text-field>
-                      <v-text-field @keyup.enter="submit" v-model="email" prepend-icon="mdi-email" name="email"
+                      <ValidationProvider name="Email" rules="required|email" mode="passive">
+                      <v-text-field slot-scope="{ errors }"
+                                    :error-messages="errors"
+                                    v-model="email"
+                                    prepend-icon="mdi-email"
+                                    name="email"
                                     label="Email"
                                     type="text"></v-text-field>
-                      <v-text-field @keyup.enter="submit" v-model="username" prepend-icon="mdi-account-circle"
-                                    name="login"
+                      </ValidationProvider>
+                      <ValidationProvider name="Username" rules="required|min:3" mode="passive">
+                      <v-text-field slot-scope="{ errors }"
+                                    :error-messages="errors"
+                                    v-model="username"
+                                    prepend-icon="mdi-account-circle"
+                                    name="username"
                                     label="Username"
                                     type="text"></v-text-field>
-                      <v-text-field @keyup.enter="submit" v-model="password" prepend-icon="mdi-lock" name="password"
-                                    label="Password" id="password" type="password"></v-text-field>
+                      </ValidationProvider>
+                      <ValidationProvider name="Password" rules="required|min:6" mode="passive">
+                      <v-text-field slot-scope="{ errors }"
+                                    :error-messages="errors"
+                                    v-model="password"
+                                    prepend-icon="mdi-lock"
+                                    name="password"
+                                    label="Password"
+                                    id="password"
+                                    type="password"></v-text-field>
+                      </ValidationProvider>
                     </v-form>
-                    <v-btn @click.prevent="submit" large color="primary">Submit</v-btn>
+                    <v-card-actions>
+                    <v-btn @click="submit" color="primary">Sign up</v-btn>
+                  </v-card-actions>
                 </v-card-text>
               </div>
             </v-expand-transition>
           </v-card>
+          </ValidationObserver>
         </v-flex>
       </v-layout>
     </v-container>
@@ -82,13 +123,16 @@
 
 <script lang="ts">
 
-import { Component, Prop, Vue } from 'vue-property-decorator'
-import { ValidationProvider, ValidationObserver, setInteractionMode } from 'vee-validate'
+import { Component, Vue } from 'vue-property-decorator'
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate'
+import { required, email, min } from 'vee-validate/dist/rules'
 
 @Component({
   components: {
     ValidationProvider,
     ValidationObserver
+  },
+  computed: {
   }
 })
 export default class Login extends Vue {
@@ -96,20 +140,35 @@ export default class Login extends Vue {
     observer: InstanceType<typeof ValidationObserver>;
 }
 
-  @Prop(String) username!: string
-  @Prop(String) password!: string
-  @Prop(String) firstName!: string
-  @Prop(String) lastName!: string
-  @Prop(String) email!: string
+  username: string
+  password: string
+  firstName: string
+  lastName: string
+  email: string
   loginButtonClicked: boolean
   registerButtonClicked: boolean
   toolbarText = 'Please enter your credentials'
 
   constructor() {
     super()
-    setInteractionMode('eager')
+    this.username = this.password = this.firstName = this.lastName = this.email = ''
     this.loginButtonClicked = true
     this.registerButtonClicked = false
+
+    extend('required', {
+      ...required,
+      message: '{_field_} is required'
+    })
+
+    extend('email', {
+      ...email,
+      message: 'This is not a valid email address'
+    })
+
+    extend('min', {
+      ...min,
+      message: '{_field_} must be at least {length} characters long'
+    })
   }
 
   get loginError(): boolean {
